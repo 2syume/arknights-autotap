1f65d7fc189d618f9d33c796212071e973b05255
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-08-12 00:53:53 -0400

    Add obsidian counter and farm plan switch

--- of_r.py
+++ of_r.py
@@ -5,7 +5,16 @@ driver = ConfiguredDriver()
 
 try:
     while True:
-        driver.farm_map("OF-6", times=3)
+        obsidian_query = driver.query_set("of_r.maps.obsidian_count")
+        if not obsidian_query:
+            driver.goto_map("OF-6")
+            obsidian_query = driver.query_set("of_r.maps.obsidian_count")
+        obs_count = int(obsidian_query[0])
+        print("- Current Obsidian count: {}".format(obs_count))
+        farm_plan = ("OF-6", 3)
+        if obs_count >= 1060:
+            farm_plan = ("OF-8", 1)
+        driver.farm_map(*farm_plan)
         driver.farm_map("OF-F3")
 except KeyboardInterrupt:
     pass

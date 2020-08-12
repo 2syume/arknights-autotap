fe85c12ab5c15e68c46311c10426548225fefbf8
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2020-08-12 01:03:26 -0400

    Optimize OF script

--- of_r.py
+++ of_r.py
@@ -5,6 +5,8 @@ driver = ConfiguredDriver()
 
 try:
     while True:
+        driver.farm_map("OF-F3")
+
         driver.refresh_screen()
         obsidian_query = driver.query_set("of_r.maps.obsidian_count")
         if not obsidian_query:
@@ -12,10 +14,10 @@ try:
             obsidian_query = driver.query_set("of_r.maps.obsidian_count")
         obs_count = int(obsidian_query[0])
         print("- Current Obsidian count: {}".format(obs_count))
+
         farm_plan = ("OF-6", 3)
         if obs_count >= 1060:
             farm_plan = ("OF-8", 1)
         driver.farm_map(*farm_plan)
-        driver.farm_map("OF-F3")
 except KeyboardInterrupt:
     pass

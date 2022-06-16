11fab7ff4dfb11d7249a2593a8028a34fddd1b4c
KangJing Huang (Chaserhkj) <huangkangjing@gmail.com>
2022-06-16 06:48:34 -0400

    Debug prelude for console

---
+++ console.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+%load_ext autoreload
+%autoreload 2
+from adbutils import adb
+import autotap
+d = adb.device()
+def show_crop(x, y, xp, yp, th=200, chi=True):
+    crop = autotap.crop_screen(d, (x, y, xp, yp))
+    crop.show()
+    mono_crop = autotap.monochrome_threshold(crop, th, True)
+    mono_crop.show()
+    if chi:
+        text = autotap.image_to_string(mono_crop, config="--psm 7", lang='chi_sim')
+    else:
+        text = autotap.image_to_string(mono_crop, config="--psm 7")
+    return text
